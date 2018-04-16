9b43fbdec7f44ab60ff383ea85cae2d283729825
Henri Chataing <henri.chataing@provenrun.com>
2018-04-16 14:35:18 +0200

    Remove licenses

--- src/types.h
+++ src/types.h
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2017-2017 Prove & Run S.A.S
- * All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Prove & Run S.A.S ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered
- * into with Prove & Run S.A.S
- *
- * PROVE & RUN S.A.S MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. PROVE & RUN S.A.S SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
-/**
- * @file
- * @brief
- * @author Henri Chataing
- * @date July 04th, 2017 (creation)
- * @copyright (c) 2017-2017, Prove & Run and/or its affiliates.
- *   All rights reserved.
- */
 
 #ifndef _TYPE_H_INCLUDED_
 #define _TYPE_H_INCLUDED_
